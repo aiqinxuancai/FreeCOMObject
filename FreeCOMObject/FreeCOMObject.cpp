62bc3d1e172393285c92d4417d024ce372b8f450
aiqinxuancai <aiqinxuancai@163.com>
2023-10-06 18:40:18 +0800

    添加项目文件。

---
+++ FreeCOMObject/FreeCOMObject.cpp
@@ -0,0 +1,28 @@
+﻿// FreeCOMObject.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "FreeCOMObject.h"
+#include <ole2.h>
+
+// 这是导出变量的一个示例
+//FREECOMOBJECT_API int nFreeCOMObject=0;
+
+// 这是导出函数的一个示例。
+void ReleaseCOMObject(void* p)
+{
+    IUnknown* pUnknown = reinterpret_cast<IUnknown*>(p);
+         if (pUnknown != nullptr)
+         {
+         pUnknown->Release();
+         }
+}
+
+
+
+//// 这是已导出类的构造函数。
+//CFreeCOMObject::CFreeCOMObject()
+//{
+//    return;
+//}
---
+++ FreeCOMObject/FreeCOMObject.h
@@ -0,0 +1,22 @@
+﻿// 下列 ifdef 块是创建使从 DLL 导出更简单的
+// 宏的标准方法。此 DLL 中的所有文件都是用命令行上定义的 FREECOMOBJECT_EXPORTS
+// 符号编译的。在使用此 DLL 的
+// 任何项目上不应定义此符号。这样，源文件中包含此文件的任何其他项目都会将
+// FREECOMOBJECT_API 函数视为是从 DLL 导入的，而此 DLL 则将用此宏定义的
+// 符号视为是被导出的。
+#ifdef FREECOMOBJECT_EXPORTS
+#define FREECOMOBJECT_API __declspec(dllexport)
+#else
+#define FREECOMOBJECT_API __declspec(dllimport)
+#endif
+
+//// 此类是从 dll 导出的
+//class FREECOMOBJECT_API CFreeCOMObject {
+//public:
+//	CFreeCOMObject(void);
+//	// TODO: 在此处添加方法。
+//};
+
+//extern FREECOMOBJECT_API int nFreeCOMObject;
+
+FREECOMOBJECT_API void ReleaseCOMObject(void* p);
